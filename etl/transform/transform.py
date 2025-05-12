cecf215419f17a18066ed09dc22f1a7542573d3f
Sam <Sammz@users.noreply.github.com>
2025-05-12 12:28:48 +0100

    add transform_data function with call to clean_house_price_data

---
+++ etl/transform/transform.py
@@ -0,0 +1,8 @@
+import pandas as pd
+from etl.transform.clean_house_price_data import clean_house_price_data
+
+
+def transform_data(data) -> pd.DataFrame:
+    cleaned_house_price_data = clean_house_price_data(data)
+
+    return clean_house_price_data
