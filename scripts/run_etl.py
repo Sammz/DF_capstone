553f9b7b842d5f18cb995911c9f32b0763c3dd4c
Sam <Sammz@users.noreply.github.com>
2025-05-13 11:25:41 +0100

    add loading stage to run_etl.py

--- scripts/run_etl.py
+++ scripts/run_etl.py
@@ -3,20 +3,23 @@ import sys
 from config.env_config import setup_env
 from etl.extract.extract import extract_data
 from etl.transform.transform import transform_data
+from etl.load.load import load_data
 
 
 def main():
     run_env_setup()
-    
+
     print("Extracting data...")
     extracted_data = extract_data()
     print("Data extraction complete.")  
-    
+
     print("Transforming data...")
     transformed_data = transform_data(extracted_data)
     print("Data transformation complete.")
-    
-    
+
+    print("Loading data...")
+    load_data(transformed_data)
+    print("Data loading complete.")
 
     print(
         f"ETL pipeline run successfully in "
