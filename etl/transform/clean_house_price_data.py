ecfdcf5cbae3e710133bc665d8ad4576828d6bf3
Sam <Sammz@users.noreply.github.com>
2025-05-13 13:22:18 +0100

    remove set_index and flake8 compliance

--- etl/transform/clean_house_price_data.py
+++ etl/transform/clean_house_price_data.py
@@ -20,22 +20,20 @@ WARNING_THRESHOLD = 10
 
 
 def clean_house_price_data(house_price_data: pd.DataFrame) -> pd.DataFrame:
-    # Set transaction_id as the index to become the primary key in the database
-    house_price_data.set_index('transaction_id')
-
     # Parse all dates with pd.to_datetime. Coerce errors means it will put
     # a null value if parsing fails.
-    house_price_data['date'] = pd.to_datetime(house_price_data['date'], 
+    house_price_data['date'] = pd.to_datetime(house_price_data['date'],
                                               errors='coerce')
 
     # Remove rows with missing values
     clean_house_price_data = remove_missing_values(house_price_data)
 
     # give newbuild column type boolean
-    clean_house_price_data['newbuild'] = clean_house_price_data['newbuild'].map({'Y': True, 'N': False})
+    clean_house_price_data['newbuild'] = clean_house_price_data[
+        'newbuild'].map({'Y': True, 'N': False})
 
     # Save the dataframe as a CSV for logging purposes
-    save_dataframe_to_csv(clean_house_price_data, "etl/data/processed/", 
+    save_dataframe_to_csv(clean_house_price_data, "etl/data/processed/",
                           "cleaned_house_price_data.csv",)
 
     return clean_house_price_data
@@ -47,13 +45,14 @@ def remove_missing_values(house_price_data):
     for col in house_price_data.columns:
         if col not in COLUMNS_ALLOWED_TO_HAVE_NULL_VALUES:
             null_count = house_price_data[col].isna().sum()
-            
+
             if null_count > 0:
                 house_price_data = house_price_data.dropna(subset=col)
                 percent_dropped = round(null_count / row_count * 100, 2)
-                
-                log_message = f'Dropped {null_count} ({percent_dropped}%) rows with missing values in the {col} column.'
-                
+
+                log_message = f'Dropped {null_count} ({percent_dropped}%)'
+                log_message += f'rows with missing values in the {col} column.'
+
                 if percent_dropped > WARNING_THRESHOLD:
                     logger.setLevel(logging.WARNING)
                     logger.warning(log_message)
