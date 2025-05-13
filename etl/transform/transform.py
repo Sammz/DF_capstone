39fe1f145bced100f731bbcb3f21ffdd6e44fdba
Sam <Sammz@users.noreply.github.com>
2025-05-13 22:58:11 +0100

    add data enrichment and cleaning, clean up code, add logging

--- etl/transform/transform.py
+++ etl/transform/transform.py
@@ -1,6 +1,6 @@
 import pandas as pd
-from etl.transform.clean_house_price_data import clean_house_price_data
+from etl.transform.clean_house_price_data import transform_house_price_data
 
 
 def transform_data(data) -> pd.DataFrame:
-    return clean_house_price_data(data)
+    return transform_house_price_data(data)
